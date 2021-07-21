1b8b5d63fec028169111dd69020d1ff026595beb
James Oswald <jtoswald@albany.edu>
2021-07-21 18:42:56 -0400

    reupdate settings to log plots

--- sumOfUniforms.py
+++ sumOfUniforms.py
@@ -7,13 +7,12 @@ import math
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-sampleSize = tf.TensorShape(1000000)
-numUniforms = 12  #The number of uniforms to sum
+sampleSize = tf.TensorShape(100000000)
 uniformSum = tf.zeros(sampleSize)
-for _ in range(numUniforms):
+for _ in range(12):
     uniformSum += tf.random.uniform(sampleSize, dtype=tf.float32)
-uniformSum = uniformSum - (numUniforms/2)
+uniformSum = uniformSum - 6
 
 plt.hist(uniformSum.numpy(), bins=1000)
-#plt.yscale('log')
+plt.yscale('log')
 plt.savefig(f"results/{os.path.basename(__file__)}.png")
